0c0c5b7c21986c8bfa60c1d959a50496cf8a2d69
JoelGhub <font.joel@gmail.com>
2022-07-08 18:34:28 +0000

    home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../../img/logo.jpeg";
 export const Navbar = () => {
+	
+	function handleClickFire(){
+	Swal.fire({
+		title: 'Inicia sesión con tu usuario:',
+		input: 'text',
+		title2: 'Inicia sesión con tu contraseña:',
+		input: 'text',
+		inputAttributes: {
+		  autocapitalize: 'off'
+		},
+		showCancelButton: true,
+		confirmButtonText: 'Look up',
+		showLoaderOnConfirm: true,
+		preConfirm: (login) => {
+		  return fetch(`//api.github.com/users/${login}`)
+			.then(response => {
+			  if (!response.ok) {
+				throw new Error(response.statusText)
+			  }
+			  return response.json()
+			})
+			.catch(error => {
+			  Swal.showValidationMessage(
+				`Request failed: ${error}`
+			  )
+			})
+		},
+		allowOutsideClick: () => !Swal.isLoading()
+	  }).then((result) => {
+		if (result.isConfirmed) {
+		  Swal.fire({
+			title: `${result.value.login}'s avatar`,
+			title2: `${result.value.login}'s avatar`,
+			imageUrl: result.value.avatar_url
+		  })
+		}
+	  })
+	}
+	
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<>
+		<nav className="container">
+			<div className=" d-flex w-100 m-0 p-0">
+				<div style={{width:"33%"}} className="">
+					<ol className="mt-4 d-flex justify-content-around">
+						<li className="link"><Link to="#"  className="mt-2 nav-item">Inicio</Link></li>
+						<li className="link"><Link to="#"  className="mt-2 nav-item" >Imagenes</Link></li>
+						<li className="link"><Link to="#"  className="mt-2 nav-item">Recomendaciones</Link></li>
+					</ol>
 				</div>
+				<div style={{width:"33%"}} className=" p-1  m-auto text-center items-navbar">
+					{/* <img style={{width:"33%"}} className="text-center image-navbar" src={logo} alt="" /> */}
+					{/* <h1>Ruta Triple B</h1> */}
+				</div >
+					
+				<div style={{width:"33%"}} className="d-flex justify-content-end items-navbar">
+					<button type="button" className="btn btn-success btn-sm h-50 m-3" onClick={handleClickFire}>Iniciar sesión
+					
+					
+					</button>
+					<button type="button" className="btn btn-success btn-sm h-50 m-3" >Registrarse</button>
+				</div >
 			</div>
 		</nav>
+		<div  style={{width:"33%"}} className=" p-1  m-auto text-center items-navbar">
+					{/* <img style={{width:"33%"}} className="text-center image-navbar" src={logo} alt="" /> */}
+					<h1 id="title">Ruta Triple B</h1>
+		</div>
+		</>
 	);
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,65 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-
+import Swal from 'sweetalert2'
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  
+  return (
+    <>
+	
+      <div className="text-center mt-0">
+        <p>
+          <img
+            className="img-center auto img-fluid w-100"
+            src="https://images5.alphacoders.com/815/815875.jpg"
+          />
+        </p>
+        <div className="container">
+          <h1>Descripción</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas ipsam
+            praesentium tenetur? Reprehenderit veritatis, dignissimos corporis
+            debitis asperiores sit aspernatur. Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Consectetur recusandae optio molestiae
+            nemo cum tempore esse. Dolorum ab veritatis nobis voluptatum fuga,
+            provident labore magnam?
+          </p>
+        </div>
+      </div>
+      <div className="p-1 d-flex border border-danger mini-container">
+        <img
+          className="border border-dark w-25 "
+          src="https://www.imor.es/wp-content/uploads/2017/09/imagen-de-prueba-320x240-300x225.jpg"
+          alt=""
+        />
+        <div className="p-1 w-25 border border-dark">
+          <h2 className="border border-dark">CASA PACO </h2>
+          <span className="fs-6"> ❤❤❤❤❤</span>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae,
+            delectus. Deleniti, expedita distinctio blanditiis quas voluptas
+            laboriosam recusandae molestias nisi?
+          </p>
+        </div>
+      </div>
+      <div className="p-1 d-flex flex-row-reverse border border-danger mini-container">
+        <img
+          className="border border-dark w-25 "
+          src="https://www.imor.es/wp-content/uploads/2017/09/imagen-de-prueba-320x240-300x225.jpg"
+          alt=""
+        />
+        <div className="p-1 w-25 border border-dark">
+          <h2 className="border border-dark">CASA PEPE </h2>
+          <span className="fs-6">❤❤❤❤❤</span>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae,
+            delectus. Deleniti, expedita distinctio blanditiis quas voluptas
+            laboriosam recusandae molestias nisi?
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
