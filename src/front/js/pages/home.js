eb7b389bbbca7fffabd68fd69e3c1a6688cedab9
CarolinaQOTF <carolinacelis012@gmail.com>
2022-07-11 13:24:09 +0000

    Api Claudinary integrada

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
+  const [uploadImages, setUploadImages] = useState("");
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className=" container text-center mt-5">
+      <div className="btn-foto mb-3">
+        <input
+          className="form-control input-foto bg-primary text-white"
+          type="file"
+          id="formFile"
+          onChange={(e) => setUploadImages(e.target.files[0])}
+        ></input>
+      </div>
+
+      <button
+        className="btn btn-primary"
+        variant="contained"
+        onClick={(e) => actions.uploadFile(uploadImages)}
+      >
+        Subir Foto
+      </button>
+    </div>
+  );
 };
